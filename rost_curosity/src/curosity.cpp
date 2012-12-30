631bf834c0a76e5b8c448aa6c5cd4469fac5ba2a
Yogesh Girdhar <yogesh@cim.mcgill.ca>
2012-12-30 14:34:08 -0500

    cell_perplexity is now in correct space, local_surprise_1d is now max instead of mean over y axis

--- rost_curosity/src/curosity.cpp
+++ rost_curosity/src/curosity.cpp
@@ -4,12 +4,14 @@
 #include <rost_common/LocalSurprise.h>
 #include <rost_common/LocalSurprise1D.h>
 #include <vector>
+#include <algorithm>
 using namespace std;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 ros::Publisher local_surprise_pub, local_surprise1d_pub;
 
 vector<int> topic_weights;
+int total_topic_weights;
 vector<float> surprise_grid;
 vector<int> surprise_grid_centers;
 vector<int> surprise_grid_radii;
@@ -18,6 +20,7 @@ int img_width, img_height, cell_size, s_width, s_height;
 void topic_weights_callback(rost_common::TopicWeights::Ptr  msg){
   topic_weights.resize(msg->weight.size());
   copy(msg->weight.begin(), msg->weight.end(), topic_weights.begin());
+  total_topic_weights = accumulate(topic_weights.begin(), topic_weights.end(),0);
 }
 
 void topics_callback(rost_common::WordObservation::Ptr  msg){
@@ -25,26 +28,27 @@ void topics_callback(rost_common::WordObservation::Ptr  msg){
 
   assert(msg->word_pose.size() == 2* msg->words.size());
   //decay the surprise score
-  transform(surprise_grid.begin(), surprise_grid.end(), surprise_grid.begin(), 
-	    [](float s){
-	      return s*0.5;
-	    });
+  //  transform(surprise_grid.begin(), surprise_grid.end(), surprise_grid.begin(), 
+  //	    [](float s){
+  //	      return s*0.5;
+  //	    });
 
   vector<float> current_sg(surprise_grid.size(),0);
   vector<int> sg_count(surprise_grid.size(),0);
 
-  float log_P_topic = log(1.0/msg->vocabulary_size);
+  float log_P_topic_prior = log(1.0/msg->vocabulary_size);
   for(size_t i=0;i<msg->words.size(); ++i){
     int x = msg->word_pose[i*2]/cell_size;
     int y = msg->word_pose[i*2+1]/cell_size;
     int z = msg->words[i];
-    float P_topic_given_data = 1.0/(static_cast<float>(topic_weights[z])+1);
-    current_sg[static_cast<size_t>(y*s_width + x)]+= log(P_topic_given_data) - log_P_topic;
+    //    float P_topic_given_data = 1.0/(static_cast<float>(topic_weights[z]));
+    float P_topic_given_data = static_cast<float>(topic_weights[z])/total_topic_weights;
+    current_sg[static_cast<size_t>(y*s_width + x)]+= log(P_topic_given_data);
     sg_count[static_cast<size_t>(y*s_width + x)]++;
   }
 
   for(size_t i=0;i<surprise_grid.size(); ++i){
-    surprise_grid[i]= 0.5*surprise_grid[i] + 0.5*current_sg[i]/sg_count[i];
+    surprise_grid[i]= exp(-current_sg[i]/sg_count[i]);
   }
 
   rost_common::LocalSurprise::Ptr local_surprise_msg(new rost_common::LocalSurprise);
@@ -53,6 +57,7 @@ void topics_callback(rost_common::WordObservation::Ptr  msg){
   local_surprise_msg->centers = surprise_grid_centers;
   local_surprise_msg->radii = surprise_grid_radii;
   local_surprise_msg->surprise = surprise_grid;
+
   local_surprise_pub.publish(local_surprise_msg);
 
   rost_common::LocalSurprise1D::Ptr local_surprise1d_msg(new rost_common::LocalSurprise1D);
@@ -62,7 +67,8 @@ void topics_callback(rost_common::WordObservation::Ptr  msg){
   //add up the vertical axis surprise to get x axis surprise
   for(int j=0; j<s_height; ++j){
     for(int i=0;i<s_width; ++i){
-      local_surprise1d_msg->surprise[i]+=surprise_grid[iter++];
+      //      local_surprise1d_msg->surprise[i]+=surprise_grid[iter++];
+      local_surprise1d_msg->surprise[i] = max(local_surprise1d_msg->surprise[i],surprise_grid[iter++]);
     }
   }
   local_surprise1d_pub.publish(local_surprise1d_msg);
