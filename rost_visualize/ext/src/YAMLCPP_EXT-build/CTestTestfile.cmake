0d8ed1feaf402ba2ff3569195e341253488ac30f
Arnold Kalmbach <arnold.kalmbach@mail.mcgill.ca>
2013-01-23 11:44:27 -0500

    removed autogenerated yaml-cpp files

--- rost_visualize/ext/src/YAMLCPP_EXT-build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arnold/fuerte_workspace/sandbox/rost/rost_visualize/ext/src/YAMLCPP_EXT
-# Build directory: /home/arnold/fuerte_workspace/sandbox/rost/rost_visualize/ext/src/YAMLCPP_EXT-build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(test)
-SUBDIRS(util)
--- rost_visualize/ext/src/YAMLCPP_EXT-build/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arnold/fuerte_workspace/sandbox/rost/rost_visualize/ext/src/YAMLCPP_EXT/util
-# Build directory: /home/arnold/fuerte_workspace/sandbox/rost/rost_visualize/ext/src/YAMLCPP_EXT-build/util
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
