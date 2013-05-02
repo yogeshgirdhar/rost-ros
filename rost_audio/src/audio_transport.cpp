28266247a50df6ab985df63dc9be3bcbfd698682
Arnold Kalmbach <arnold.kalmbach@mail.mcgill.ca>
2013-05-01 23:54:05 -0400

    added a wav file for sanity checks

--- rost_audio/src/audio_transport.cpp
+++ rost_audio/src/audio_transport.cpp
@@ -24,26 +24,27 @@ namespace audio_transport
         nhp.param<int>("samplerate", samplerate, 44100);
 	nhp.param<string>("audiosource", source_string, "");
 
-        _pub = _nh.advertise<rost_audio::AudioRaw>("audio", 10, true);
+        _pub = _nh.advertise<rost_audio::AudioRaw>("audio", 5, true);
 
         _loop = g_main_loop_new(NULL, false);
         	
         // We create the sink first, just for convenience
 	_sink = gst_element_factory_make("appsink", "sink");
 	g_object_set(G_OBJECT(_sink), "emit-signals", true, NULL);
-	g_object_set(G_OBJECT(_sink), "max-buffers", 100, NULL);
+	g_object_set(G_OBJECT(_sink), "max-buffers", 1000, NULL);
 	g_signal_connect( G_OBJECT(_sink), "new-buffer", 
 			  G_CALLBACK(onNewBuffer), this);
 
 	GError ** error;
 	char config[256];
 	if (source_string.compare("") == 0 or source_string.compare("alsasrc") == 0) {
-	  sprintf(config, "alsasrc ! audioconvert ! audio/x-raw-int, format=U16BE, rate=%d, channels=1", samplerate);
+	  sprintf(config, "alsasrc ! audioconvert ! audio/x-raw-int, format=U16, rate=%d, channels=1 ! audioresample", samplerate);
 	}
 	else {
-	  cout << "Using " << source_string.c_str() << " as audio source" << endl;
-	  sprintf(config, "filesrc location=%s ! decodebin ! audioconvert ! audio/x-raw-int, format=S16BE, rate=%d, channels=1", source_string.c_str(), samplerate);
+ 	  sprintf(config, "filesrc location=%s ! decodebin ! audioconvert ! audio/x-raw-int, format=U16, rate=%d, channels=1 ! audioresample", source_string.c_str(), samplerate);
 	}
+	cout << "starting pipeline" << endl;
+	cout << config << endl;
 	_pipeline = gst_parse_launch(config, error);
 	if (_pipeline == NULL)
 	{
@@ -86,12 +87,43 @@ namespace audio_transport
 
         GstBuffer *buffer;
         g_signal_emit_by_name(appsink, "pull-buffer", &buffer);
-
+        
         rost_audio::AudioRaw msg;
-        msg.data.resize( buffer->size );
-        memcpy( &msg.data[0], buffer->data, buffer->size);
+	
+	
+	for (int i = 0; i < buffer->size; i+=2){
+	  long val = 0;
+	  for (int j = 0; j < 2; j++){
+	    val += (buffer->data[j + i] << (j*8));
+	  }
+	  cout << val << endl;
+	}
+	/*double out;
+	int consec_zeros = 0;
+	int size;
+	while (size*2 < buffer->size){
+	  long val = 0;
+	  for (int i = 0; i < 2; i++){
+	    val += (buffer->data[size + i] << ((1-i)*8));
+	  }
+	  if (val == (1 << 15)){
+	    consec_zeros++;
+	    if (consec_zeros >= 5){
+	      for (int j = 0; j < 5; j++){
+		msg.data.pop_back();
+	      }
+	      break;
+	    }
+	  }
+	  else{
+	    consec_zeros = 0;
+	  }
+	  out = ((double) val / (1 << 15));
+	  msg.data.push_back(out);
+	  size++;
+	}
         msg.samplerate = server->samplerate;
-        msg.size = buffer->size;
+        msg.size = size;*/
 
         server->publish(msg);
 
@@ -100,6 +132,7 @@ namespace audio_transport
 
     public:
       int samplerate;
+      int precision;
       string source_string;
     private:
       ros::NodeHandle _nh;
